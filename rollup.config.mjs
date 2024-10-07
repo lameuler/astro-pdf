5a8a0fcad78d4dffb28231209cf6dc34075333d6
eu ler <27113373+lameuler@users.noreply.github.com>
2024-10-07 11:19:58 +0800

    move to esm only

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'rollup'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import { builtinModules } from 'module'
-import { join } from 'path'
 import virtual from '@rollup/plugin-virtual'
 import { readFile } from 'fs/promises'
 
@@ -10,21 +9,11 @@ const version = JSON.parse(await readFile('package.json', 'utf-8')).version
 
 export default defineConfig({
     input: 'src/index.ts',
-    output: [
-        {
-            dir: 'dist',
-            format: 'cjs',
-            sourcemap: true,
-            entryFileNames: '[format]/[name].js'
-        },
-        {
-            dir: 'dist',
-            format: 'es',
-            sourcemap: true,
-            entryFileNames: '[format]/[name].js',
-            plugins: [pluginEmitPackageJSON('es')]
-        }
-    ],
+    output: {
+        dir: 'dist',
+        format: 'es',
+        sourcemap: true
+    },
     external: [...builtinModules, /node_modules/],
     plugins: [
         resolve(),
@@ -36,17 +25,3 @@ export default defineConfig({
         })
     ]
 })
-
-// create a package.json file for node to recognise es modules
-function pluginEmitPackageJSON(dir = '') {
-    return {
-        name: 'plugin-emit-package-json',
-        generateBundle() {
-            this.emitFile({
-                type: 'asset',
-                fileName: join(dir, 'package.json'),
-                source: '{"type":"module"}'
-            })
-        }
-    }
-}
