444aef40e9095c567b2b3011140f0e65468a8a1b
lam eu ler <27113373+lameuler@users.noreply.github.com>
2024-10-13 21:08:31 +0800

    add eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 /** @type {import('eslint').Linter.Config} */
 export default [
@@ -8,5 +9,6 @@ export default [
     { ignores: ['dist/*', 'test/fixtures/**/*.d.ts'] },
     { languageOptions: { globals: globals.node } },
     pluginJs.configs.recommended,
-    ...tseslint.configs.recommended
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier
 ]
