a6e1c054d26b2569101c1a9701e5506723f58b66
eu ler <27113373+lameuler@users.noreply.github.com>
2024-10-13 20:10:23 +0800

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+    { files: [ "**/*.{js,mjs,cjs,ts}" ] },
+    { ignores: [ "dist/*", "test/fixtures/**/*.d.ts" ] },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended
+];
