9aff024e570fd15685266d3f7a8807b76b1beb4d
lam eu ler <27113373+lameuler@users.noreply.github.com>
2024-10-13 21:08:19 +0800

    add eslint config typedef

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
+/** @type {import('eslint').Linter.Config} */
 export default [
     { files: ['**/*.{js,mjs,cjs,ts}'] },
     { ignores: ['dist/*', 'test/fixtures/**/*.d.ts'] },
