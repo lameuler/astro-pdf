47f5286acdf9c4683b3c89e32e131a323d93b9a8
lam eu ler <27113373+lameuler@users.noreply.github.com>
2024-10-17 16:12:40 +0800

    test index export

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { virtualVersion } from './rollup.config.mjs'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    test: {
+        dir: './test'
+    },
+    plugins: [virtualVersion]
+})
